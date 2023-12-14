b0eb37ea9dd3af91ba07b613346e7829beb08418
Szőcs Áron <szocs.aa@gmail.com>
2023-12-14 15:25:59 +0100

    init lion plugin

---
+++ LionPlugin/LionPlugin/Lion.cs
@@ -0,0 +1,17 @@
+﻿namespace LionPlugin
+{
+    public class Lion
+    {
+        public string Name { get; set; } = "Lion";
+
+        public void Add()
+        {
+            Console.WriteLine($"{Name} was added to the Zoo.");
+        }
+        
+        public void Sound()
+        {
+            Console.WriteLine($"{Name} goes roar.");
+        }
+    }
+}
